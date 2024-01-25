009e6b670a1719f05199b212dcb1f2dcdeef960f
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2024-01-25 16:10:03 +0900

    Update item_locustfile.py

--- item_locustfile.py
+++ item_locustfile.py
@@ -8,8 +8,8 @@ class ItemsUser(HttpUser):
     def get_items(self):
         headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkwMDAzNSwianRpIjoiYzA4MzRiYjAtNmRmZC00MzczLTlmYTktM2Q5YmIyZjk4MTQzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6OTUsIm5iZiI6MTcwNTkwMDAzNSwiZXhwIjoxNzA4NDkyMDM1fQ.41qawHivs_LRtr9bwzC3Q4Lmkb4A2mYVnVtLqGPi4x0",
-            "X-CSRF-TOKEN": "IjFkZGU0ZDhjZmQ2NGZmYzBmZWI5MWIxODA4NWRlOWEwODlkOTFjZTYi.Za34Aw.Jz9gu9h4gw0SQKymY8LctPB_PUI"
+            "Authorization": "",
+            "X-CSRF-TOKEN": ""
             }
         self.client.get('/categories/items', headers=headers)
 
@@ -18,8 +18,8 @@ class ItemsUser(HttpUser):
     def post_item(self):
         headers = {
             'Content-Type': 'application/json',
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkwMDAzNSwianRpIjoiYzA4MzRiYjAtNmRmZC00MzczLTlmYTktM2Q5YmIyZjk4MTQzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6OTUsIm5iZiI6MTcwNTkwMDAzNSwiZXhwIjoxNzA4NDkyMDM1fQ.41qawHivs_LRtr9bwzC3Q4Lmkb4A2mYVnVtLqGPi4x0",
-            "X-CSRF-TOKEN": "IjFkZGU0ZDhjZmQ2NGZmYzBmZWI5MWIxODA4NWRlOWEwODlkOTFjZTYi.Za34Aw.Jz9gu9h4gw0SQKymY8LctPB_PUI"
+            "Authorization": "",
+            "X-CSRF-TOKEN": ""
         }
         payload = {
             'name': f'New Item {random.randint(1, 4000)}',
@@ -30,29 +30,6 @@ class ItemsUser(HttpUser):
         assert response.status_code in [201, 409]
 
 
-    # @task
-    # def put_item(self):
-    #     headers = {
-    #         "Content-Type": "application/json",
-    #         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkwMDAzNSwianRpIjoiYzA4MzRiYjAtNmRmZC00MzczLTlmYTktM2Q5YmIyZjk4MTQzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6OTUsIm5iZiI6MTcwNTkwMDAzNSwiZXhwIjoxNzA4NDkyMDM1fQ.41qawHivs_LRtr9bwzC3Q4Lmkb4A2mYVnVtLqGPi4x0",
-    #         "X-CSRF-TOKEN": "IjFkZGU0ZDhjZmQ2NGZmYzBmZWI5MWIxODA4NWRlOWEwODlkOTFjZTYi.Za34Aw.Jz9gu9h4gw0SQKymY8LctPB_PUI"
-    #         }
-    #     item_id = 1  # replace with a valid item ID
-    #     data = {
-    #         'name': 'Updated Item',  # replace with a valid updated name
-    #         'price': 25.0  # replace with a valid updated price
-    #     }
-    #     self.client.put(f'/categories/items/{item_id}', json=data, headers=headers)
-
-    # @task
-    # def delete_item(self):
-    #     headers = {
-    #         "Content-Type": "application/json",
-    #         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkwMDAzNSwianRpIjoiYzA4MzRiYjAtNmRmZC00MzczLTlmYTktM2Q5YmIyZjk4MTQzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6OTUsIm5iZiI6MTcwNTkwMDAzNSwiZXhwIjoxNzA4NDkyMDM1fQ.41qawHivs_LRtr9bwzC3Q4Lmkb4A2mYVnVtLqGPi4x0",
-    #         "X-CSRF-TOKEN": "IjFkZGU0ZDhjZmQ2NGZmYzBmZWI5MWIxODA4NWRlOWEwODlkOTFjZTYi.Za34Aw.Jz9gu9h4gw0SQKymY8LctPB_PUI"
-    #         }
-    #     item_id = 1  # replace with a valid item ID
-    #     self.client.delete(f'/categories/items/{item_id}', headers=headers)
 
     
     @task
@@ -64,8 +41,8 @@ class ItemsUser(HttpUser):
     def post_cost(self):
         headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkwMDAzNSwianRpIjoiYzA4MzRiYjAtNmRmZC00MzczLTlmYTktM2Q5YmIyZjk4MTQzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6OTUsIm5iZiI6MTcwNTkwMDAzNSwiZXhwIjoxNzA4NDkyMDM1fQ.41qawHivs_LRtr9bwzC3Q4Lmkb4A2mYVnVtLqGPi4x0",
-            "X-CSRF-TOKEN": "IjFkZGU0ZDhjZmQ2NGZmYzBmZWI5MWIxODA4NWRlOWEwODlkOTFjZTYi.Za34Aw.Jz9gu9h4gw0SQKymY8LctPB_PUI"
+            "Authorization": "",
+            "X-CSRF-TOKEN": ""
             }
         data = {
             'items': [
@@ -74,14 +51,3 @@ class ItemsUser(HttpUser):
             ]
         }
         self.client.post('/categories/items/costs', json=data, headers=headers)
-
-    # @task
-    # def update_cost(self):
-    #     headers = {
-    #         "Content-Type": "application/json",
-    #         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTkwMDAzNSwianRpIjoiYzA4MzRiYjAtNmRmZC00MzczLTlmYTktM2Q5YmIyZjk4MTQzIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6OTUsIm5iZiI6MTcwNTkwMDAzNSwiZXhwIjoxNzA4NDkyMDM1fQ.41qawHivs_LRtr9bwzC3Q4Lmkb4A2mYVnVtLqGPi4x0",
-    #         "X-CSRF-TOKEN": "IjFkZGU0ZDhjZmQ2NGZmYzBmZWI5MWIxODA4NWRlOWEwODlkOTFjZTYi.Za34Aw.Jz9gu9h4gw0SQKymY8LctPB_PUI"
-    #         }
-    #     item_id = 1  # replace with a valid item ID
-    #     data = {'cost': 6.0}  # replace with a valid updated cost
-    #     self.client.put(f'/categories/items/costs/{item_id}', json=data, headers=headers)
